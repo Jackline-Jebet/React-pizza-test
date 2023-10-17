69182329922ca29a14d876f249d2a0ba548b2337
Jackline-Jebet <jebetjackline2016@gmail.com>
2023-10-17 09:16:24 +0300

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,10 @@ function Navbar() {
       <div className='leftSide' id={openLinks ? "open" : "close"}>
       <img src={Logo} alt='logo' />
       <div className='hiddenLinks'>
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/contact">Contact</Link> */}
       </div>
       </div>
       <div className='rightSide'>
